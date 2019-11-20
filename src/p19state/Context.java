c94eb253ebecb11cc0595895c670d35593cb0bc6
LiYotA <LiYotA@126.com>
2019-11-20 08:26:15 +0800

    State状态模式；

---
+++ src/p19state/Context.java
@@ -0,0 +1,8 @@
+package p19state;
+
+public interface Context {
+    void setClock(int hour);
+    void changeState(State state);
+    void callSecurityCenter(String msg);
+    void recordLog(String msg);
+}
---
+++ src/p19state/DayState.java
@@ -0,0 +1,31 @@
+package p19state;
+
+public class DayState implements State {
+    private static DayState singleton = new DayState();
+    private DayState() { }
+    public static DayState getInstance() {
+        return singleton;
+    }
+    @Override
+    public void doClock(Context context, int hour) {
+        if (hour < 9 || hour >= 17) {
+            context.changeState(NightState.getInstance()); //改变状态
+        }
+    }
+    @Override
+    public void doUse(Context context) {
+        context.recordLog("使用金库(白天)");
+    }
+    @Override
+    public void doAlarm(Context context) {
+        context.callSecurityCenter("按下警铃(白天)");
+    }
+    @Override
+    public void doPhone(Context context) {
+        context.callSecurityCenter("正常通话(白天)");
+    }
+    @Override
+    public String toString() {
+        return "[白天]";
+    }
+}
